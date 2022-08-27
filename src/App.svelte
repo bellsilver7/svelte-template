d8a439a57447d0b54229940b208555ecc7400874
bellsilver7 <bellsilver7@gmail.com>
2022-08-27 11:14:35 +0900

    앱 페이지 수정

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
   import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import typescriptLogo from './assets/typescript.svg'
+  import tailwindcssLogo from './assets/tailwindcss.svg'
 </script>
 
 <main>
+  <h1 class="text-3xl font-bold underline bg-red-400">
+    Hello world!
+  </h1>
   <div>
     <a href="https://vitejs.dev" target="_blank"> 
       <img src="/vite.svg" class="logo" alt="Vite Logo" />
@@ -11,20 +15,14 @@
     <a href="https://svelte.dev" target="_blank"> 
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
+    <a href="https://typescript.dev" target="_blank"> 
+      <img src={typescriptLogo} class="logo typescript" alt="Typescript Logo" />
+    </a>
+    <a href="https://tailwindcss.dev" target="_blank"> 
+      <img src={tailwindcssLogo} class="logo tailwindcss" alt="TailwindCSS Logo" />
+    </a>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <h1>Vite + Svelte + Typescript + TailwindCSS</h1>
 </main>
 
 <style>
@@ -39,7 +37,10 @@
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
   }
-  .read-the-docs {
-    color: #888;
+  .logo.typescript:hover {
+    filter: drop-shadow(0 0 2em #007accaa);
+  }
+  .logo.tailwindcss:hover {
+    filter: drop-shadow(0 0 2em #00bfa5aa);
   }
 </style>
